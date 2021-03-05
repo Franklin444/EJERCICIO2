0ac0419f5cb6676492578fb31ca76a75b93db8c9
FRANKLIN ALEXANDER ZETINO HERNANDEZ <fzetinoh@miumg.edu.gt>
2021-03-05 16:04:01 -0600

    Agregar archivos de proyecto.

---
+++ Y GUESS YOUR BIRTHDAY/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Y_GUESS_YOUR_BIRTHDAY
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Y GUESS YOUR BIRTHDAY/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Y_GUESS_YOUR_BIRTHDAY
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            var happy = (Convert.ToInt32 (Final.Text)-365);
+
+            DisplayAlert("Tu fecha de cumpleaños es...", happy.ToString(), "Te adivine verdad");
+             
+            Fin.Text =happy.ToString();
+
+            hola.Text = Fin.Text.Substring(0,2);
+            hol.Text = Fin.Text.Substring(Fin.Text.Length -2);
+        }
+    }
+}
+
